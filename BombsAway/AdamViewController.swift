8c45c3a53008a61134a5fe0805acc8fd7aad9523
Adam Rothberg <adam@ThaiChili.local>
2015-04-18 00:25:37 -0400

    x

--- BombsAway/AdamViewController.swift
+++ BombsAway/AdamViewController.swift
@@ -9,111 +9,47 @@
 import Foundation
 import UIKit
 
-class AdamViewController: UIViewController {
+class AdamViewController: UIViewController, IBombListener {
     
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var imgBomb: UIImageView!
     @IBOutlet weak var imgExplosion: UIImageView!
-    @IBAction func buttonUp(sender: AnyObject) {
-        moveBomb()
-    }
+    
     var i = 0
-    var movingBomb = false
-    let fusePlayer = AudioPlayer(filename: "fuse")
-    let boomPlayer = AudioPlayer(filename: "boom")
-    let pingPlayer = AudioPlayer(filename: "ping")
-    var bombStartingPosition : CGPoint!
+    var bomb : Bomb?
     
-    func moveBomb()
-    {
-        if (movingBomb) {
-            return
-        }
-        
-        movingBomb = true
-        imgExplosion.hidden = true
-        imgBomb.hidden = false
-        let random = Misc.GetRandom(0, endingInclusive: 3)
-        switch(random)
-        {
-        case 0:
-            bombStartingPosition = CGPoint(x: self.view.center.x, y: -imgBomb.frame.height / 2)
-        case 1:
-            bombStartingPosition = CGPoint(x: self.view.frame.width + imgBomb.frame.width / 2, y: self.view.center.y)
-        case 2:
-            bombStartingPosition = CGPoint(x: self.view.center.x, y: imgBomb.frame.height / 2 + self.view.frame.height)
-        case 3:
-            bombStartingPosition = CGPoint(x: -imgBomb.frame.width / 2, y: self.view.center.y)
-        default:
-            var e = NSException(name:"name", reason:"invalid case", userInfo:nil)
-            e.raise()
-        }
-        imgBomb.center = bombStartingPosition
-
-        UIView.animateWithDuration(1,
-            animations: { () -> Void in
-            // do actual move
-            self.fusePlayer.play()
-            self.imgBomb.center = self.view.center
-            },
-            completion: { (complete) -> Void in
-                if (complete)
-                {
-                    //when animation completes
-                    self.imgBomb.hidden = true
-                    self.imgExplosion.center = self.imgBomb.center
-                    self.imgExplosion.hidden = false
-                    self.imgExplosion.alpha = 0
-                    self.fusePlayer.stop()
-                    self.boomPlayer.play()
-                    self.imgExplosion.fadeIn(duration: 0.2, completion: { (completed) -> Void in
-                            self.imgExplosion.fadeOut(duration: 0.5)
-                        })
-                    self.movingBomb = false
-                    self.resendBomb()
-                }
-        })
+    func onDiffusedAndSent(bomb: Bomb) {
+        newIncomingBomb()
     }
     
-    func resendBomb()
-    {
-        var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("moveBomb"), userInfo: nil, repeats: false)
+    func onExploded(bomb: Bomb) {
+        newIncomingBomb()
     }
-
+    
+    func newIncomingBomb()
+    {
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("newIncomingBombImpl"), userInfo: nil, repeats: false)
+    }
+    func newIncomingBombImpl()
+    {
+        bomb = Bomb(listener: self, uiView: self.view, imgBomb: imgBomb, imgExplosion: imgExplosion)
+        bomb!.incoming()
+    }
+    
     override func shouldAutorotate() -> Bool {
         return false
     }
     
     @IBAction func tapBomb(sender: UIGestureRecognizer) {
-        if (movingBomb)
+        if (bomb != nil)
         {
             let p = sender.locationInView(self.view)
-            let hit = imgBomb.layer.presentationLayer().hitTest(p) != nil
-            println("hit\(i++)? \(hit)")
-            if (hit)
-            {
-                let layer = imgBomb.layer.presentationLayer() as! CALayer
-                let frame = layer.frame
-                imgBomb.layer.removeAllAnimations()
-                imgBomb.frame = frame
-            
-                UIView.animateWithDuration(0.1,
-                    animations: { () -> Void in
-                        // do actual move
-                        self.pingPlayer.play()
-                        self.imgBomb.center = self.bombStartingPosition
-                    },
-                    completion: { (complete) -> Void in
-                        if (complete)
-                        {
-                            //when animation completes
-                            self.pingPlayer.stop()
-                            self.movingBomb = false
-                            self.resendBomb()
-                        }
-                })
-            }
+            bomb!.tryDiffuseAndSend(p)
         }
+    }
+    
+    @IBAction func buttonUp(sender: AnyObject) {
+        newIncomingBomb()
     }
     
     override func supportedInterfaceOrientations() -> Int {
---
+++ BombsAway/Bomb.swift
@@ -0,0 +1,110 @@
+//
+//  Bomb.swift
+//  BombsAway
+//
+//  Created by Adam Rothberg on 4/17/15.
+//  Copyright (c) 2015 Crazy8Dev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol IBombListener {
+    //events
+    func onExploded(bomb : Bomb)
+    func onDiffusedAndSent(bomb : Bomb)
+}
+
+class Bomb
+{
+    let uiView : UIView!
+    let imgBomb : UIImageView!
+    let imgExplosion : UIImageView!
+    let listener : IBombListener!
+ 
+    let fusePlayer = AudioPlayer(filename: "fuse")
+    let boomPlayer = AudioPlayer(filename: "boom")
+    let pingPlayer = AudioPlayer(filename: "ping")
+    var bombStartingPosition : CGPoint!
+    
+    func incoming()
+    {
+        imgExplosion.hidden = true
+        imgBomb.hidden = false
+        let random = Misc.GetRandom(0, endingInclusive: 3)
+        switch(random)
+        {
+        case 0:
+            bombStartingPosition = CGPoint(x: uiView.center.x, y: -imgBomb.frame.height / 2)
+        case 1:
+            bombStartingPosition = CGPoint(x: uiView.frame.width + imgBomb.frame.width / 2, y: uiView.center.y)
+        case 2:
+            bombStartingPosition = CGPoint(x: uiView.center.x, y: imgBomb.frame.height / 2 + uiView.frame.height)
+        case 3:
+            bombStartingPosition = CGPoint(x: -imgBomb.frame.width / 2, y: uiView.center.y)
+        default:
+            var e = NSException(name:"name", reason:"invalid case", userInfo:nil)
+            e.raise()
+        }
+        imgBomb.center = bombStartingPosition
+        
+        UIView.animateWithDuration(1,
+            animations: { () -> Void in
+                // do actual move
+                self.fusePlayer.play()
+                self.imgBomb.center = self.uiView.center
+            },
+            completion: { (complete) -> Void in
+                if (complete)
+                {
+                    //when animation completes
+                    self.imgBomb.hidden = true
+                    self.imgExplosion.center = self.imgBomb.center
+                    self.imgExplosion.hidden = false
+                    self.imgExplosion.alpha = 0
+                    self.fusePlayer.stop()
+                    self.boomPlayer.play()
+                    self.imgExplosion.fadeIn(duration: 0.2, completion: { (completed) -> Void in
+                    self.imgExplosion.fadeOut(duration: 0.5)
+                    self.listener.onExploded(self)
+                })
+            }
+        })
+    }
+    
+    func tryDiffuseAndSend(point : CGPoint!)
+    {
+        let hit = imgBomb.layer.presentationLayer().hitTest(point) != nil
+        if (hit)
+        {
+            let layer = imgBomb.layer.presentationLayer() as! CALayer
+            let frame = layer.frame
+            imgBomb.layer.removeAllAnimations()
+            imgBomb.frame = frame
+            
+            UIView.animateWithDuration(0.3,
+                animations: { () -> Void in
+                    // do actual move
+                    self.pingPlayer.play()
+                    self.imgBomb.center = self.bombStartingPosition
+                },
+                completion: { (complete) -> Void in
+                    if (complete)
+                    {
+                        //when animation completes
+                        self.pingPlayer.stop()
+                        self.listener.onDiffusedAndSent(self)
+                    }
+            })
+        }
+    }
+    
+    init(listener : IBombListener, uiView : UIView, imgBomb : UIImageView, imgExplosion : UIImageView)
+    {
+        self.listener = listener
+        self.uiView = uiView
+        self.imgBomb = imgBomb
+        self.imgExplosion = imgExplosion
+    }
+    
+}
