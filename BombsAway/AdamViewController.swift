15b5f662151ad9e2343f250ae458f3ab7dcfa7e8
Adam Rothberg <adam@ThaiChili.local>
2015-04-18 12:06:37 -0400

    game master connection

--- BombsAway/AdamViewController.swift
+++ BombsAway/AdamViewController.swift
@@ -9,15 +9,30 @@
 import Foundation
 import UIKit
 
-class AdamViewController: UIViewController, IBombListener {
+class AdamViewController: UIViewController, IBombListener, IGameClient, UITextFieldDelegate {
     
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var imgBomb: UIImageView!
     @IBOutlet weak var imgExplosion: UIImageView!
+    @IBOutlet weak var txtName: UITextField!
     
     var i = 0
     var bomb : Bomb?
+    var gameMaster = GameMaster.sharedInstance
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        gameMaster.registerClient(self.nameOfClass, client: self)
+    }
     
+    // MARK: text field
+    func textFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: bomb events
     func onDiffusedAndSent(bomb: Bomb) {
         self.bomb = nil
         newIncomingBomb()
@@ -37,10 +52,6 @@ class AdamViewController: UIViewController, IBombListener {
         }
     }
     
-    override func shouldAutorotate() -> Bool {
-        return false
-    }
-    
     @IBAction func tapBomb(sender: UIGestureRecognizer) {
         if (bomb != nil)
         {
@@ -49,8 +60,32 @@ class AdamViewController: UIViewController, IBombListener {
         }
     }
     
+    // MARK: GameMaster
     @IBAction func buttonUp(sender: AnyObject) {
+        gameMaster.addPlayer(self.nameOfClass, name: txtName.text)
+        button.hidden = true
+        txtName.hidden = true
+    }
+    
+    func onBombed(bomb: FBBomb) {
         newIncomingBomb()
+    }
+    func onBombAppeared(bomb: FBBomb) {
+        
+    }
+    func onBombDisappeared(bomb: FBBomb) {
+        
+    }
+    func onPlayerAppeared(player: FBPlayer) {
+        
+    }
+    func onPlayerDisappeared(player: FBPlayer) {
+        
+    }
+    
+    // MARK: Standard iOS Stuff
+    override func shouldAutorotate() -> Bool {
+        return false
     }
     
     override func supportedInterfaceOrientations() -> Int {
@@ -59,10 +94,11 @@ class AdamViewController: UIViewController, IBombListener {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtName.delegate = self; // ADD THIS LINE
         imgBomb.hidden = true
         imgExplosion.hidden = true
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
